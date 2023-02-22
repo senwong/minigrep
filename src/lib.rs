9bf1330d19991f1b8ee7acf944c32b2e62ce9c65
wangshaosen <wangshaosen@meituan.com>
2023-02-22 10:01:43 +0800

    change main

--- src/lib.rs
+++ src/lib.rs
@@ -68,21 +68,6 @@ pub fn search_case_insensitive<'a>(query: &'a str, contents: &'a str) -> Vec<&'a
 }
 
 
-/// Adds one to the number given.
-///
-/// # Examples
-///
-/// ```
-/// let arg = 5;
-/// let answer = my_crate::add_one(arg);
-///
-/// assert_eq!(6, answer);
-/// ```
-pub fn add_one(x: i32) -> i32 {
-    x + 1
-}
-
-
 #[cfg(test)]
 mod tests {
 
